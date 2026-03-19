09fafb12150ff5d7990a0d8d0af5ca970e725f3f
Snehal Thakur <snehal.thakur@in.ncs-i.com>
2026-03-19 12:13:49 +0530

    Exploring GET,POST,DELETE

--- src/app.js
+++ src/app.js
@@ -2,13 +2,29 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test",(req, res) => {
-  res.send("Hello from the server");
+app.get("/user", (req, res) => {
+  res.send({
+    name: "John Doe",
+    city: "New York",
+  });
 });
 
-app.use("/namaste",(req,res)=>{
-    res.send("Namaste from the server");
-})
+app.post("/user", (req, res) => {
+  console.log("save data to database");
+  res.send("Data saved successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("User deleted successfully");
+});
+
+app.put("/user", (req, res) => {
+  res.send("User updated successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("User partially updated successfully");
+});
 
 app.listen(7777, () => {
   console.log("server is running on port 7777");
