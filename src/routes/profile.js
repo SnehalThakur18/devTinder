bc02bc6fda8deefaad41beabed0caf33f14cc3d5
SnehalThakur18 <snehalmthakur182.com>
2026-03-28 19:11:15 +0530

    Create logout, edit profile, edit password APIs

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,13 +1,99 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middleware/auth");
+const { validateEditProfileData } = require("../utils/validations");
+const validator = require("validator");
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
-    res.send(user);
+    res.json({
+      message: "Profile retrived successfully",
+      data: user,
+      status: "success",
+      statusCode: 200,
+    });
   } catch (err) {
-    res.status(400).send("ERROR: " + err.message);
+    res.status(400).json({
+      message: "ERROR: " + err.message,
+      status: "error",
+      statusCode: 400,
+    });
+  }
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    if (!validateEditProfileData(req)) {
+      return res.status(400).json({
+        message: "Invalid fields in request body.",
+        status: "error",
+        statusCode: 400,
+      });
+    }
+    const loggedInUser = req.user;
+
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+    await loggedInUser.save();
+
+    res.json({
+      message: "Profile updated successfully",
+      data: loggedInUser,
+      status: "success",
+      statusCode: 200,
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "ERROR: " + err.message,
+      status: "error",
+      statusCode: 400,
+    });
+  }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+
+    if (!currentPassword || !newPassword) {
+      return res.status(400).json({
+        message: "currentPassword and newPassword are required.",
+        status: "error",
+        statusCode: 400,
+      });
+    }
+
+    const isCurrentPasswordValid =
+      await req.user.validatePassword(currentPassword);
+    if (!isCurrentPasswordValid) {
+      return res.status(400).json({
+        message: "Current password is incorrect.",
+        status: "error",
+        statusCode: 400,
+      });
+    }
+
+    if (!validator.isStrongPassword(newPassword)) {
+      return res.status(400).json({
+        message: "New password is not strong enough.",
+        status: "error",
+        statusCode: 400,
+      });
+    }
+
+    req.user.password = await req.user.encryptPassword(newPassword);
+    await req.user.save();
+    res.json({
+      message: "Password updated successfully",
+      status: "success",
+      statusCode: 200,
+    });
+  } catch (err) {
+    res.status(400).json({
+      message: "ERROR: " + err.message,
+      status: "error",
+      statusCode: 400,
+    });
   }
 });
 
