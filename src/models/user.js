8f7db2c43ea459b27cf76fc968d8b56eb245add4
SnehalThakur18 <snehalmthakur182.com>
2026-03-28 14:15:07 +0530

    Created userSchema helper methods for JWT token creation and vaildate password logic and moved that logics to user.js file.

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema(
   {
@@ -72,6 +74,24 @@ const userSchema = new Schema(
     timestamps: true,
   },
 );
+
+userSchema.methods.getJWT = async function () {
+  const user = this;
+  const token = await jwt.sign({ _id: user._id }, "testdata@123", {
+    expiresIn: "7d",
+  });
+  return token;
+};
+
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
+  const user = this;
+  const passwordHash = user.password;
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    passwordHash,
+  );
+  return isPasswordValid;
+};
 
 const UserModel = mongoose.model("User", userSchema);
 
