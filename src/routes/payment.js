5b4eec698e1512babf7ffee23e26a864df57912e
SnehalThakur18 <snehalmthakur182@gmail.com>
2026-04-16 12:04:11 +0530

    payment updated

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -3,18 +3,22 @@ const paymentRouter = express.Router();
 const razorpayInstance = require("../utils/razorpay");
 const Payment = require("../models/payment");
 const { userAuth } = require("../middleware/auth");
+const { membershipAmount } = require("../utils/constants");
 
 paymentRouter.post("/payment/create", userAuth, async (req, res) => {
   console.log("Payment creation endpoint hit");
   try {
+    const membershipType = req.body.membershipType || "silver";
+    const { firstName, lastName, email } = req.user;
     const order = await razorpayInstance.orders.create({
-      amount: 70000, // amount in the smallest currency unit (e.g., paise for INR)
+      amount: membershipAmount[membershipType] * 100, // amount in the smallest currency unit (e.g., paise for INR)
       currency: "INR",
       receipt: "receipt#1",
       notes: {
-        firstName: "value1",
-        lastName: "value2",
-        membershipType: "silver",
+        firstName,
+        lastName,
+        email,
+        membershipType: membershipType,
       },
     });
     const payment = new Payment({
@@ -29,7 +33,9 @@ paymentRouter.post("/payment/create", userAuth, async (req, res) => {
 
     const savedPayment = await payment.save();
 
-    res.status(200).json({ ...savedPayment.toJSON() });
+    res
+      .status(200)
+      .json({ ...savedPayment.toJSON(), keyId: process.env.RAZORPAY_KEY_ID });
   } catch (err) {
     return res.status(500).json({ msg: err.message });
   }
